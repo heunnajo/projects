413caeb3252bfdf658902b403177af7e83a71c59
조흔나 <61869540+heunnajo@users.noreply.github.com>
2023-04-24 09:08:16 +0900

    Initial commit

---
+++ BBS/src/main/java/comment/Comment.java
@@ -0,0 +1,40 @@
+package comment;
+
+public class Comment {
+	private int commentID;
+	private String userID;
+	private String commentContent;
+	private String commentDate;
+	private int bbsID;
+	public int getCommentID() {
+		return commentID;
+	}
+	public void setCommentID(int commentID) {
+		this.commentID = commentID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getCommentContent() {
+		return commentContent;
+	}
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+	public String getCommentDate() {
+		return commentDate;
+	}
+	public void setCommentDate(String commentDate) {
+		this.commentDate = commentDate;
+	}
+	public int getBbsID() {
+		return bbsID;
+	}
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	
+}
