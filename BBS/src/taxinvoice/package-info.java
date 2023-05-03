30758a2f6201dc2a6d0e9f495f74b140b23540ed
heunnajo <61869540+heunnajo@users.noreply.github.com>
2023-05-03 22:34:43 +0900

    Delete package-info.java

--- BBS/src/taxinvoice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:kr:or:kec:standard:Tax:ReusableAggregateBusinessInformationEntitySchemaModule:1:0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package taxinvoice;
