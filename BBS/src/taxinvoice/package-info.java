fe190b787f3c9b779745d4f96cb503083bd07448
조흔나 <61869540+heunnajo@users.noreply.github.com>
2023-05-03 15:10:51 +0900

    Create package-info.java

---
+++ BBS/src/taxinvoice/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:kr:or:kec:standard:Tax:ReusableAggregateBusinessInformationEntitySchemaModule:1:0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package taxinvoice;
