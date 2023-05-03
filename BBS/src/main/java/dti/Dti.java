cef0fba7b4e1fb7a5b763b8f9f47fef1bbe9e962
조흔나 <61869540+heunnajo@users.noreply.github.com>
2023-05-03 09:07:20 +0900

    Create Dti.java

---
+++ BBS/src/main/java/dti/Dti.java
@@ -0,0 +1,161 @@
+package dti;
+
+public class Dti {
+	/*
+	 private int bbsID;
+	private String bbsTitle;
+	private String userID;
+	private String bbsDate;
+	private String bbsContent;
+	private int bbsAvailable;
+	 * */
+	private String conversationID;
+	private String supBuyType;
+	private String direction;
+	private String dtiDate;
+	private String dtiType;
+	private String taxDemand;
+	private String supComRegno;
+	private String supRepName;
+	private String supComName;
+	private String supComAddr;
+	private String byrComRegno;
+	private String byrRepName;
+	private String byrComName;
+	private String byrComAddr;
+	private String byrEmail;
+	private String supAmount;
+	private String taxAmount;
+	private String totalAmount;
+	private String DttYn;
+	private String amendCode;
+	private String oriIssueId;
+	public String getConversationID() {
+		return conversationID;
+	}
+	public void setConversationID(String conversationID) {
+		this.conversationID = conversationID;
+	}
+	public String getSupBuyType() {
+		return supBuyType;
+	}
+	public void setSupBuyType(String supBuyType) {
+		this.supBuyType = supBuyType;
+	}
+	public String getDirection() {
+		return direction;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	public String getDtiDate() {
+		return dtiDate;
+	}
+	public void setDtiDate(String dtiDate) {
+		this.dtiDate = dtiDate;
+	}
+	public String getDtiType() {
+		return dtiType;
+	}
+	public void setDtiType(String dtiType) {
+		this.dtiType = dtiType;
+	}
+	public String getTaxDemand() {
+		return taxDemand;
+	}
+	public void setTaxDemand(String taxDemand) {
+		this.taxDemand = taxDemand;
+	}
+	public String getSupComRegno() {
+		return supComRegno;
+	}
+	public void setSupComRegno(String supComRegno) {
+		this.supComRegno = supComRegno;
+	}
+	public String getSupRepName() {
+		return supRepName;
+	}
+	public void setSupRepName(String supRepName) {
+		this.supRepName = supRepName;
+	}
+	public String getSupComName() {
+		return supComName;
+	}
+	public void setSupComName(String supComName) {
+		this.supComName = supComName;
+	}
+	public String getSupComAddr() {
+		return supComAddr;
+	}
+	public void setSupComAddr(String supComAddr) {
+		this.supComAddr = supComAddr;
+	}
+	public String getByrComRegno() {
+		return byrComRegno;
+	}
+	public void setByrComRegno(String byrComRegno) {
+		this.byrComRegno = byrComRegno;
+	}
+	public String getByrRepName() {
+		return byrRepName;
+	}
+	public void setByrRepName(String byrRepName) {
+		this.byrRepName = byrRepName;
+	}
+	public String getByrComName() {
+		return byrComName;
+	}
+	public void setByrComName(String byrComName) {
+		this.byrComName = byrComName;
+	}
+	public String getByrComAddr() {
+		return byrComAddr;
+	}
+	public void setByrComAddr(String byrComAddr) {
+		this.byrComAddr = byrComAddr;
+	}
+	public String getByrEmail() {
+		return byrEmail;
+	}
+	public void setByrEmail(String byrEmail) {
+		this.byrEmail = byrEmail;
+	}
+	public String getSupAmount() {
+		return supAmount;
+	}
+	public void setSupAmount(String supAmount) {
+		this.supAmount = supAmount;
+	}
+	public String getTaxAmount() {
+		return taxAmount;
+	}
+	public void setTaxAmount(String taxAmount) {
+		this.taxAmount = taxAmount;
+	}
+	public String getTotalAmount() {
+		return totalAmount;
+	}
+	public void setTotalAmount(String totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	public String getDttYn() {
+		return DttYn;
+	}
+	public void setDttYn(String dttYn) {
+		DttYn = dttYn;
+	}
+	public String getAmendCode() {
+		return amendCode;
+	}
+	public void setAmendCode(String amendCode) {
+		this.amendCode = amendCode;
+	}
+	public String getOriIssueId() {
+		return oriIssueId;
+	}
+	public void setOriIssueId(String oriIssueId) {
+		this.oriIssueId = oriIssueId;
+	}
+	
+	
+}
